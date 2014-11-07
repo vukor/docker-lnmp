cdad1893ae488090373ab6f7f9567d0663e58993
vukor-work <anton@bugreev.ru>
2014-11-07 14:24:37 +0600

    modified:   docker-start.sh 	logs/

--- docker-start.sh
+++ docker-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 80:80 -v `pwd`/etc/nginx/hosts/:/etc/nginx/hosts/ -v `pwd`/www:/home/dev/www -v `pwd`/vakhr-lnmp/logs:/home/dev/logs vakhr-lnmp
+docker run -d -p 80:80 -v `pwd`/etc/nginx/hosts/:/etc/nginx/hosts/ -v `pwd`/www:/home/dev/www -v `pwd`/logs:/home/dev/logs docker-lnmp
 
 exit 0
 
