5fcdebf1e86ae21c5baaa2d554d733f4e6fa49bd
Anton Bugreev <anton@bugreev.ru>
2014-12-01 16:28:04 +0600

    fix php basename, change start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 ## start mysql
-/usr/bin/mysqld_safe --datadir=/var/lib/mysql --socket=/var/lib/mysql/mysql.sock --pid-file=/var/run/mysqld/mysqld > /dev/null 2>&1 &
+service mysqld start
 
 ## set permissions for mysql
 sleep 1
 /usr/bin/mysql -e "grant all on *.* to $MYSQL_LOGIN identified by '$MYSQL_PASSWORD';"
 
 ## start php-fpm
-/usr/sbin/php-fpm --daemonize -c /etc/php.ini
+service php-fpm start
 
 ## start nginx
-/usr/sbin/nginx -c /etc/nginx/nginx.conf
+service nginx start
 
---
+++ www/test/www/test.php
@@ -0,0 +1,3 @@
+<?php 
+echo basename("файл .txt"); 
+?>
