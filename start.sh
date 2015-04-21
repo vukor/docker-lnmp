1ce51e2206d3f6539548ba88375ad0f1bfb2d503
Anton Bugreev <anton@bugreev.ru>
2015-04-21 15:05:52 +0600

    fix connect to mysql on localhost

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ service mysqld start
 ## set permissions for mysql
 sleep 1
 /usr/bin/mysql -e "grant all on *.* to $MYSQL_LOGIN identified by '$MYSQL_PASSWORD';"
+/usr/bin/mysql -e "grant all on *.* to $MYSQL_LOGIN@localhost identified by '$MYSQL_PASSWORD';"
 
 ## start php-fpm
 service php-fpm start
